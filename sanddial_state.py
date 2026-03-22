5ab965480f2756b1b425652be27c282fd7a0bf7d
wooningeire <22846982+wooningeire@users.noreply.github.com>
2026-03-22 17:19:52 -0400

    fix: use base state

--- sanddial_state.py
+++ sanddial_state.py
@@ -1,35 +1,22 @@
 import hou
-import toolutils
+import viewerstate.utils as su
 
-class SanddialState(object):
-    """
-    Python Viewer State for Sanddial.
-    Displays a persistent prompt message to indicate the viewer state is active.
-    """
-    def __init__(self, state_name, scene_viewer):
-        self.state_name = state_name
-        self.scene_viewer = scene_viewer
-
+class State(object):
+    def __init__(self, **kwargs):
+        self.__dict__.update(kwargs)
     def onEnter(self, kwargs):
         self.scene_viewer.setPromptMessage("Sanddial Viewer State: Ready for Erodibility Paint / Environment Edit")
 
-    def onInterrupt(self, kwargs):
-        pass
-
-    def onResume(self, kwargs):
-        self.scene_viewer.setPromptMessage("Sanddial Viewer State: Ready for Erodibility Paint / Environment Edit")
-
-    def onMouseEvent(self, kwargs):
-        return False
-
 def createViewerStateTemplate():
-    """ Mandatory entry point to create and return the viewer state template to register. """
-    state_typename = "sanddial_state"
-    state_label = "Sanddial State"
+    """ Mandatory entry point to create and return the viewer state 
+        template to register. """
+
+    state_typename = kwargs["type"].definition().sections()["DefaultState"].contents()
+    state_label = "V::sanddial::1.0"
     state_cat = hou.sopNodeTypeCategory()
 
     template = hou.ViewerStateTemplate(state_typename, state_label, state_cat)
-    template.bindFactory(SanddialState)
-    template.bindIcon("MISC_python")
+    template.bindFactory(State)
+    template.bindIcon(kwargs["type"].icon())
 
     return template
